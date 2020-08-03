cb852f8c999dcd19e7b8372f7dad9e9e0b7ad99a
seth-d-mclaughlin <seth-d-mclaughlin@gmail.com>
2020-08-03 15:39:00 -0400

    Componets setup

---
+++ 24hour/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"; // we're going to be using these dependicies from within our project so we need to access those 
+import "./Navbar.css";
+
+const Navbar = (props) => {
+  return(
+    <div>
+      <nav>
+       <h1>24-hour</h1>
+        
+      </nav>
+    </div>
+  )
+}
+export default Navbar;
