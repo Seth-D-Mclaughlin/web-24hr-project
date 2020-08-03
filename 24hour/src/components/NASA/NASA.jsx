cd2175c16afdb6d4ba8b06a8d22f6bc3a388b7b7
seth-d-mclaughlin <seth-d-mclaughlin@gmail.com>
2020-08-03 16:13:15 -0400

    NASA setup & UserInfo

---
+++ 24hour/src/components/NASA/NASA.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./NASA.css";
+
+
+const NASA = (props) => {
+    return(
+        <div>
+            <h3>Enter longitude and Latitude below.</h3>
+        </div>
+    )
+}
+
+export default NASA;
